1e84b1e49b7a2cd5835bb4376a2f9c337bfa9adf
Adil Sadout <adelsadout@hotmail.com>
2021-08-05 15:22:19 +0100

    added nft fetch

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ const chainSelectContainer = document.querySelector("#chainSelectedContainer");
 const disconnectBtn = document.querySelector("#disconnect_button");
 const chainSelectedList = document.querySelector("#chainSelected");
 const userTokensContainer = document.querySelector("#userTokensContainer");
+const userNFTsContainer = document.querySelector("#userNFTsContainer");
 const userTokensTable = document.querySelector("#userTokens");
+const userNFTGrid = document.querySelector("#nftGrid");
 const SpinnerIcon2 = document.querySelector("#spinnerIcon2");
 
 async function login() {
@@ -19,29 +21,7 @@ async function login() {
         chainSelectContainer.classList.toggle("d-none");
         disconnectBtn.classList.toggle("d-none");
         alert("Wallet Connected");
-        
-        chainSelectedList.addEventListener("change", async event =>{
-            SpinnerIcon2.classList.add("spinner-border");
-            userTokensContainer.classList?.remove("d-none");
-            userTokensTable.innerHTML = "";
-            let Balances = await Moralis.Web3.getAllERC20({chain: event.currentTarget.value});
-            
-            
-            Balances.forEach(token =>{
-                userTokensTable.insertAdjacentHTML("afterbegin", `
-
-                <tr>
-                    <td>${token.name}</td>
-                    <td>${token.symbol}</td>
-                    <td>${Number.parseFloat(token.balance)/10**token.decimals}</td>
-                </tr>
-
-            `)
-            })
-
-            SpinnerIcon2.classList.remove("spinner-border");
-            
-        })
+        return currentUser;
     } catch (error) {
         SpinnerIcon2.classList.remove("spinner-border");
         alert("Error: Cannot Fetch User Balance")
@@ -49,13 +29,13 @@ async function login() {
     }
 }
 
-
 async function logout() {
     try {
         let currentUser = null;
         loginBtn.classList.toggle("d-none");
         disconnectBtn.classList.toggle("d-none");
         userTokensContainer.classList.add("d-none");
+        userNFTsContainer.classList.add("d-none");
         chainSelectContainer.classList.add("d-none");
         alert("Wallet Disconnected");
     } catch (error) {
@@ -63,8 +43,95 @@ async function logout() {
     }
 }
 
-loginBtn.addEventListener("click", event =>{
-    login();
+async function fetchUserPortfolio(){
+    chainSelectedList.addEventListener("change", async event =>{
+        SpinnerIcon2.classList.add("spinner-border");
+        userTokensContainer.classList?.remove("d-none");
+        userNFTsContainer.classList?.remove("d-none");
+        userTokensTable.innerHTML = "";
+        let Balances = await fetchToken();
+        let userNFTs = await fetchNFT();
+        
+        Balances.forEach(token =>{
+            userTokensTable.insertAdjacentHTML("afterbegin", `
+
+            <tr>
+                <td>${token.name}</td>
+                <td>${token.symbol}</td>
+                <td>${Number.parseFloat(token.balance)/10**token.decimals}</td>
+            </tr>
+
+        `)
+        })
+        if (userNFTs.length > 0){
+            userNFTs.forEach(async nft =>{
+                try{
+                    let response = await fetch(nft.token_uri,{
+                        mode: 'cors',
+                        header:{
+                            'Access-Control-Allow-Origin': '*',
+                            'Control-Allow-Origin': '*'
+                        }
+
+                });
+                    let nftObject = await response.json();
+                    console.log(nftObject);
+                    console.log(nft);
+
+                    userNFTGrid.insertAdjacentHTML("afterbegin", `
+    
+                <div class="col">
+                    <div class="card my-5 mx-auto" style="width: 18rem;">
+                        <img src="${nftObject.image}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${nftObject.name}</h5>
+                            <p class="card-text">Token Address: ${nft.token_address}</p>
+                        </div>
+                        <div class="card-body">
+                            <a href="${nftObject.external_url}" class="card-link link-info">NFT URL</a>
+                        </div>
+                    </div>
+                </div>
+
+            `)
+
+                }catch(err){
+                    alert(err)
+                }
+                
+                
+            })
+        }else{
+            userNFTGrid.insertAdjacentHTML("afterbegin", `
+    
+                No NFTs Were Found
+    
+            `)
+        }
+        
+
+
+        SpinnerIcon2.classList.remove("spinner-border");
+        
+    })
+}
+
+async function fetchToken(){
+    let Balances = await Moralis.Web3.getAllERC20({chain: event.currentTarget.value});
+    console.log(Balances);
+    return Balances
+}
+
+async function fetchNFT(){
+    let userNFTs = await Moralis.Web3.getNFTs({chain:chainSelectedList.value});
+    console.log(userNFTs);
+    return userNFTs
+}
+
+loginBtn.addEventListener("click", async event =>{
+    currentUserConnected = await login();
+    console.log(currentUserConnected)
+    fetchUserPortfolio();
 })
 
 disconnectBtn.addEventListener("click", event =>{
